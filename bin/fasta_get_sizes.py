7e01925aff9ac242b313f63e5f616d1223c425ea
Sylvain FORET <sylvain.foret@anu.edu.au>
2015-08-31 09:40:19 +1000

    Added fasta_get_sizes

---
+++ bin/fasta_get_sizes.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+import fasta_utils.io
+
+
+def getArgs():
+    parser = argparse.ArgumentParser(description='Gets the list of sequence sizes')
+    parser.add_argument('input',
+                        nargs='*',
+                        help='Input fasta file(s)')
+    parser.add_argument('-o',
+                        '--output',
+                        metavar='OUT',
+                        help='Output file')
+    args = parser.parse_args()
+    return args
+
+def main():
+    args    = getArgs()
+    inputs  = [None]
+    maxSize = args.smaller_than
+    minSize = args.larger_than
+    out     = sys.stdout
+    if args.output:
+        out = openMaybeCompressed(args.output, 'w')
+    if args.input:
+        inputs = args.input
+    for path in inputs:
+        for header, sequence in fasta_utils.io.iterFasta(path):
+            length = len(sequence)
+            out.write('%d\n' % length)
+    if args.output:
+        out.close()
+
+if __name__ == '__main__':
+    main()
